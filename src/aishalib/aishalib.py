80a4bbefb95a58ce771b7d348cf1556930425ee0
man4j <man4j@ya.ru>
2024-07-31 16:01:47 +0300

    fixes

--- src/aishalib/aishalib.py
+++ src/aishalib/aishalib.py
@@ -18,6 +18,7 @@ class Aisha:
                 prompt = f.read()
 
         config = AutoConfig.from_pretrained(base_model)
+        self.first_user_req_template = None
 
         match config.model_type:
             case "qwen2":
@@ -50,6 +51,12 @@ class Aisha:
                 self.system_injection_template = "<start_of_turn>system\n{system_injection}<end_of_turn>\n"
                 self.tokens = [self.tokenizer(self.tokenizer.bos_token + f"<start_of_turn>system\n{prompt}<end_of_turn>\n")["input_ids"]]
                 self.stop_token = "<end_of_turn>"
+            case "mistral":
+                self.generation_promp_template = " "
+                self.user_req_template = "[INST] {user_req}[/INST]"
+                self.first_user_req_template = " {user_req}[/INST]"
+                self.tokens = [self.tokenizer(f"[INST] {prompt}")["input_ids"]]
+                self.stop_token = "</s>"
             case _:
                 raise RuntimeError("Unknown model: " + config.model_type)
 
@@ -58,30 +65,39 @@ class Aisha:
         self.llm_backend.base_model = base_model
         self.llm_backend.tokenizer = self.tokenizer
 
-        self.generation_prompt_tokens = self.tokenizer(self.generation_promp_template)["input_ids"]
+        self.generation_prompt_tokens = self.tokenize(self.generation_promp_template)
         logger.info("System prompt size: " + str(len(self.tokens[0])))
+
+    def tokenize(self, text):
+        tokens = self.tokenizer(text)["input_ids"]
+        if self.tokenizer.bos_token_id:
+            tokens.remove(self.tokenizer.bos_token_id)
+        return tokens
 
     def sanitize(self, text):
         return text.replace("#", "").replace("<|", "").replace("|>", "")
 
     def add_user_request(self, user_request, system_injection="", meta_info=""):
-        text = self.user_req_template.replace("{user_req}", self.sanitize(user_request.strip()) + meta_info)
+        if self.first_user_req_template and len(self.tokens) == 1:
+            text = self.first_user_req_template.replace("{user_req}", self.sanitize(user_request.strip()) + meta_info)
+        else:
+            text = self.user_req_template.replace("{user_req}", self.sanitize(user_request.strip()) + meta_info)
         if system_injection:
             text += self.system_injection_template.replace("{system_injection}", system_injection)
-        tokens = self.tokenizer(text)["input_ids"]
+        tokens = self.tokenize(text)
         self.tokens.append(tokens)
         self._cut_context()  # Освобождаем место под ответ модели
 
     def add_system_injection(self, system_injection):
         text = self.system_injection_template.replace("{system_injection}", system_injection)
-        self.tokens.append(self.tokenizer(text)["input_ids"])
+        self.tokens.append(self.tokenize(text))
         self._cut_context()  # Освобождаем место под ответ модели
 
     def completion(self, temp=0.0, top_p=0.5):
         request_tokens = sum(self.tokens, [])
         request_tokens += self.generation_prompt_tokens
         text_resp = self.llm_backend.completion(request_tokens, temp, top_p)
-        response_tokens = self.tokenizer(text_resp.strip() + self.stop_token)["input_ids"]
+        response_tokens = self.tokenize(text_resp.strip() + self.stop_token)
         response_tokens = self.generation_prompt_tokens + response_tokens
         self.tokens.append(response_tokens)
         return text_resp
@@ -90,7 +106,7 @@ class Aisha:
         request_tokens = sum(self.tokens, [])
         request_tokens += self.generation_prompt_tokens
         text_resp = await self.llm_backend.stream_completion(request_tokens, callback, temp, top_p)
-        response_tokens = self.tokenizer(text_resp.strip() + self.stop_token)["input_ids"]
+        response_tokens = self.tokenize(text_resp.strip() + self.stop_token)
         response_tokens = self.generation_prompt_tokens + response_tokens
         self.tokens.append(response_tokens)
         return text_resp
@@ -110,7 +126,9 @@ class Aisha:
         if free_tokens < self.max_predict:
             while free_tokens < self.max_predict * 8:  # обрезаем с большим запасом, чтобы кеш контекста работал лучше
                 free_tokens += len(self.tokens[1])
-                del self.tokens[1]
+                del self.tokens[1]  # del user request
+                free_tokens += len(self.tokens[1])
+                del self.tokens[1]  # del model response
 
     def clear_context(self):
         del self.tokens[1:]
--- src/aishalib/utils.py
+++ src/aishalib/utils.py
@@ -4,7 +4,7 @@ import pytz
 from datetime import datetime
 
 
-def get_time_string(tz = 'Europe/Moscow'):
+def get_time_string(tz='Europe/Moscow'):
     locale.setlocale(locale.LC_ALL, ('ru_RU', 'UTF-8'))
     dt = datetime.now(pytz.timezone(tz))
     return dt.strftime(" ### Текущее время: %A, %d %B, %Yг. %H:%M")
